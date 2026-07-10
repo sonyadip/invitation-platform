b2b75944b03afa72d319e7632245d2b91d9bb4f8
Sony <sonypratama190301@gmail.com>
2026-07-10 21:41:53 +0800

    switch astro adapter to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
